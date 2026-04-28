6c1f50aae498f3830037870fa58e01f96bbba64e
Sujatha <navkaurgill95@gmail.com>
2026-04-28 21:11:34 +0530

    Point frontend to hosted backend

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://127.0.0.1:8000";
+const BACKEND_URL = "https://petvilla-api-x833.onrender.com";
 export const API = `${BACKEND_URL}/api`;
 
 export const api = axios.create({
