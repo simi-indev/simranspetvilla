1f35023459f5168cde1a9a6ea645611be419577d
simi-indev <simran.kaurgill9@gmail.com>
2026-05-01 10:46:42 +0000

    lazy load all routes to fix TBT

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,52 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import HomePage from "./pages/HomePage";
-import ServicesIndexPage from "./pages/ServicesIndexPage";
-import ServicePage from "./pages/ServicePage";
-import AboutPage from "./pages/AboutPage";
-import ReviewsPage from "./pages/ReviewsPage";
-import BlogPage from "./pages/BlogPage";
-import BlogPostPage from "./pages/BlogPostPage";
-import ContactPage from "./pages/ContactPage";
-import BookingPage from "./pages/BookingPage";
-import AdminLoginPage from "./pages/AdminLoginPage";
-import AdminDashboardPage from "./pages/AdminDashboardPage";
-import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
-import TermsPage from "./pages/TermsPage";
-import RefundPolicyPage from "./pages/RefundPolicyPage";
 import { Toaster } from "sonner";
 import { BusinessInfoProvider } from "./lib/businessInfo";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const ServicesIndexPage = lazy(() => import("./pages/ServicesIndexPage"));
+const ServicePage = lazy(() => import("./pages/ServicePage"));
+const AboutPage = lazy(() => import("./pages/AboutPage"));
+const ReviewsPage = lazy(() => import("./pages/ReviewsPage"));
+const BlogPage = lazy(() => import("./pages/BlogPage"));
+const BlogPostPage = lazy(() => import("./pages/BlogPostPage"));
+const ContactPage = lazy(() => import("./pages/ContactPage"));
+const BookingPage = lazy(() => import("./pages/BookingPage"));
+const AdminLoginPage = lazy(() => import("./pages/AdminLoginPage"));
+const AdminDashboardPage = lazy(() => import("./pages/AdminDashboardPage"));
+const PrivacyPolicyPage = lazy(() => import("./pages/PrivacyPolicyPage"));
+const TermsPage = lazy(() => import("./pages/TermsPage"));
+const RefundPolicyPage = lazy(() => import("./pages/RefundPolicyPage"));
 
 function App() {
   return (
     <BusinessInfoProvider>
       <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/services" element={<ServicesIndexPage />} />
-            <Route path="/services/:slug" element={<ServicePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/reviews" element={<ReviewsPage />} />
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="/blog/:slug" element={<BlogPostPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/book" element={<BookingPage />} />
-            <Route path="/admin" element={<AdminLoginPage />} />
-            <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-            <Route path="/terms" element={<TermsPage />} />
-            <Route path="/refund-policy" element={<RefundPolicyPage />} />
-          </Route>
-        </Routes>
+        <Suspense fallback={<div style={{ minHeight: "100vh" }} />}>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/services" element={<ServicesIndexPage />} />
+              <Route path="/services/:slug" element={<ServicePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/reviews" element={<ReviewsPage />} />
+              <Route path="/blog" element={<BlogPage />} />
+              <Route path="/blog/:slug" element={<BlogPostPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/book" element={<BookingPage />} />
+              <Route path="/admin" element={<AdminLoginPage />} />
+              <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+              <Route path="/terms" element={<TermsPage />} />
+              <Route path="/refund-policy" element={<RefundPolicyPage />} />
+            </Route>
+          </Routes>
+        </Suspense>
         <Toaster position="top-center" richColors />
       </BrowserRouter>
     </BusinessInfoProvider>
   );
 }
-
 export default App;
-// force rebuild Thu Apr 30 08:30:02 UTC 2026
-
